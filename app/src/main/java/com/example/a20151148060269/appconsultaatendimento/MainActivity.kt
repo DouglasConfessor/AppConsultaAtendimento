b859a46641acab68d25fad766df407c727872acd
douglasconfessor <douglasconfessor@gmail.com>
2018-11-23 17:32:43 -0300

    Criação da segunda tela

--- app/src/main/java/com/example/a20151148060269/appconsultaatendimento/MainActivity.kt
+++ app/src/main/java/com/example/a20151148060269/appconsultaatendimento/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.a20151148060269.appconsultaatendimento
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun entrar(view: View){
+        val usuario:String=editText.text.toString()
+        val numerosus:String=editText2.text.toString()
+        val senha:String=editText3.text.toString()
+
+        if (senha=="1234"){
+            val intentCall:Intent=Intent(this,MainActivity2::class.java)
+            val parametros:Bundle=Bundle()
+            parametros.putString("usuario",usuario)
+            parametros.putString("numerosus",numerosus)
+            intentCall.putExtras(parametros)
+            startActivity(intentCall)
+        }else{
+            Toast.makeText(this,"Senha Errada",Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+
+
 }
